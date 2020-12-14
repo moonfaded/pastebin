51fe0db24931a946e838644577431fbf85df82dd
Aliaksandr_Vladyka <aliaksandr_vladyka@epam.com>
2020-12-14 09:09:10 +0300

    stage2 pastebin integration

---
+++ src/test/java/test/ICanWinTest.java
@@ -0,0 +1,37 @@
+package test;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+import page.NewPastebinPage;
+import page.PastebinHomePage;
+
+public class ICanWinTest {
+  private final String PASTE_NAME = "helloweb";
+  private final String CODE = "Hello from WebDriver";
+  private WebDriver driver;
+
+  @BeforeTest
+  public void setupBrowser() {
+    driver = new ChromeDriver();
+    driver.manage().window().maximize();
+  }
+
+  @Test
+  public void createNewPasteHelloWeb() {
+    NewPastebinPage newPage = new PastebinHomePage(driver)
+                                  .openPage()
+                                  .addCode(CODE)
+                                  .selectPasteExpirationAsTenMinutes()
+                                  .addPasteNameOrTitle(PASTE_NAME)
+                                  .createNewPaste();
+  }
+
+  @AfterTest
+  public void closeBrowser() {
+    driver.quit();
+    driver = null;
+  }
+}
