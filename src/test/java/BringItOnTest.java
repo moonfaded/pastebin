e760e69278ccad7804d9dbd86ca7a4f5928ddead
Aliaksandr_Vladyka <aliaksandr_vladyka@epam.com>
2020-12-14 15:41:16 +0300

    checkIfReceivedTitleMatchesPasteName

--- src/test/java/BringItOnTest.java
+++ src/test/java/BringItOnTest.java
@@ -1,10 +1,12 @@
 import com.codeborne.selenide.Selenide;
 import org.openqa.selenium.By;
+import org.testng.Assert;
 import org.testng.annotations.Test;
 
 public class BringItOnTest {
 
-  @Test
+  private static final String PASTE_NAME = "how to gain dominance among developers";
+
   public static void bringItOn() {
     Selenide.open("https://pastebin.com");
     Selenide.$(By.id("postform-text")).sendKeys("git config --global user.name  \"New Sheriff in Town\"\n"
@@ -13,7 +15,23 @@ public class BringItOnTest {
     Selenide.$(By.id("select2-postform-format-container")).click();
     Selenide.$(By.xpath("//li[contains(text(), 'Bash')]")).click();
     Selenide.$(By.xpath("//li[contains(text(), '10 Minutes')]")).click();
-    Selenide.$(By.id("postform-name")).sendKeys("Selenide.$(By.id(\"postform-name\")).sendKeys(\"helloweb\");");
+    Selenide.$(By.id("postform-name")).setValue(PASTE_NAME);
     Selenide.$(By.xpath("//button[contains(text(), 'Create New Paste')]")).click();
   }
+
+  @Test
+  public void checkIfReceivedTitleMatchesPasteName() {
+    Assert.assertEquals(Selenide.$(By.xpath("//div[@class = 'info-top']/h1")).getValue(), PASTE_NAME);
+  }
+
+  @Test
+  public void checkIfSyntaxIsHighlighted() {
+
+  }
+
+  @Test
+  public void checkIfReceivedTextMatchesCode() {
+
+  }
+
 }
