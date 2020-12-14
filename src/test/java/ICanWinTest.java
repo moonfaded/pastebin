7544bae7902ddcc89d0aab01cfc9b39dfd5cbe7c
Aliaksandr_Vladyka <aliaksandr_vladyka@epam.com>
2020-12-14 15:22:22 +0300

    I can win task done

--- src/test/java/ICanWinTest.java
+++ src/test/java/ICanWinTest.java
@@ -11,6 +11,9 @@ public class ICanWinTest {
   public static void iCanWin() {
     Selenide.open("https://pastebin.com");
     Selenide.$(By.id("postform-text")).sendKeys("Hello from WebDriver");
-//    Selenide.$(By.id("select2-postform-expiration-container")).selectOptionByValue("String value");
+    Selenide.$(By.id("select2-postform-expiration-container")).click();
+    Selenide.$(By.xpath("//li[contains(text(), '10 Minutes')]")).click();
+    Selenide.$(By.id("postform-name")).sendKeys("helloweb");
+    Selenide.$(By.xpath("//button[contains(text(), 'Create New Paste')]")).click();
   }
 }
